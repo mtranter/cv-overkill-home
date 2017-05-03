984032494590e0b35f9ee779f18bcff265e22f0c
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-03 21:06:49 +0100

    Add scripts folder back in

---
+++ src/scripts/app-bundle.js
@@ -0,0 +1,114 @@
+define('app',['exports', 'aurelia-framework'], function (exports, _aureliaFramework) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.App = undefined;
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var _dec, _class;
+
+  var App = exports.App = (_dec = (0, _aureliaFramework.inject)(_aureliaFramework.All.of('plugin.route')), _dec(_class = function () {
+    function App(routes) {
+      _classCallCheck(this, App);
+
+      this.message = 'Hello World!';
+      this.pluginRoutes = routes;
+    }
+
+    App.prototype.configureRouter = function configureRouter(config, router) {
+      this.router = router;
+      config.title = 'Aurelia';
+      var routes = [{ route: ['', 'home'], name: 'home', moduleId: 'home/index' }, { route: ['me'], name: 'me', moduleId: 'me/index' }].concat(this.pluginRoutes);
+
+      config.map(routes);
+    };
+
+    return App;
+  }()) || _class);
+});
+define('environment',["exports"], function (exports) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = {
+    debug: true,
+    testing: true
+  };
+});
+define('main',['exports', './environment'], function (exports, _environment) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.configure = configure;
+
+  var _environment2 = _interopRequireDefault(_environment);
+
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
+  }
+
+  Promise.config({
+    warnings: {
+      wForgottenReturn: false
+    }
+  });
+
+  function configure(aurelia) {
+    console.log(aurelia);
+    aurelia.use.standardConfiguration().feature('resources').plugin('experience/plugin');
+
+    if (_environment2.default.debug) {
+      aurelia.use.developmentLogging();
+    }
+
+    if (_environment2.default.testing) {
+      aurelia.use.plugin('aurelia-testing');
+    }
+
+    aurelia.start().then(function () {
+      return aurelia.setRoot();
+    });
+  }
+});
+define('home/index',["exports"], function (exports) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var Index = exports.Index = function Index() {
+    _classCallCheck(this, Index);
+  };
+});
+define('resources/index',["exports"], function (exports) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.configure = configure;
+  function configure(config) {}
+});
+define('text!app.html', ['module'], function(module) { module.exports = "<template>\n  <h1>${message}</h1>\n  <router-view></router-vew>\n</template>\n"; });
+define('text!home/index.html', ['module'], function(module) { module.exports = "<template>\n  <div>Home</div>\n</template>\n"; });
+//# sourceMappingURL=app-bundle.js.map
