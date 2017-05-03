f036aa75e9ade2404696ebbbdbbeb9f33643515b
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-03 17:06:30 +0100

    Remove module

--- deploy/module.tf
+++ deploy/module.tf
@@ -1,12 +1,24 @@
-module "home" {
-  source = "github.com/mtranter/tf-cv-overkill-aurelia-module"
-  website_files = [
-    "index.html",
-    "scripts/app-bundle.js",
-    "scripts/require.js",
-    "scripts/text.js",
-    "scripts/vendor-bundle.js"
-  ]
-  relative_source_path = "/../src/"
+
+
+provider "aws" {
   region = "${var.region}"
+}
+
+ data "terraform_remote_state" "cv_state" {
+   backend = "s3"
+
+   config {
+     bucket  = "cv-overkill-tf-state"
+     key     = "aws-infrastructure"
+     region  = "eu-west-1"
+   }
+ }
+
+resource "aws_s3_bucket_object" "object" {
+  count   = "${length(var.website_files)}"
+  bucket  = "${data.terraform_remote_state.cv_state.website_bucket_id}"
+  key     = "${var.website_files[count.index]}"
+  source  = "${path.cwd}${var.relative_source_path}${var.website_files[count.index]}"
+  etag    = "${md5(file("${path.cwd}${var.relative_source_path}${var.website_files[count.index]}"))}"
+  content_type = "${lookup(var.content_type_map, element(split(".", var.website_files[count.index]), length(split(".", var.website_files[count.index])) - 1))}"
 }
--- deploy/variables.tf
+++ deploy/variables.tf
@@ -1,3 +1,25 @@
 variable "region" {
   default = "eu-west-1"
 }
+variable "relative_source_path" {
+  default = "/../src/"
+}
+
+variable "website_files" {
+  type = "list"
+  default = [
+    "index.html",
+    "scripts/app-bundle.js",
+    "scripts/require.js",
+    "scripts/text.js",
+    "scripts/vendor-bundle.js"
+  ]
+}
+
+variable "content_type_map" {
+  default = {
+    "html"    = "text/html",
+    "js"      = "application/javascript",
+    "css"     = "text/css"
+  }
+}
