789df3ce7ba80df83d31f3c8824d907846b9f22c
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-09 06:38:19 +0100

    Refactor into home view!

--- src/src/home/index.js
+++ src/src/home/index.js
@@ -1,5 +1,8 @@
+import {All, inject} from 'aurelia-framework';
 
-
+@inject(All.of('plugin.route'), All.of('plugin.widget.homepage.component'))
 export class Index {
-
+  constructor(widgets) {
+    this.widgets = widgets;
+  }
 }
