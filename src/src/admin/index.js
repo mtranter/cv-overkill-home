1be79cdcb675791bda9ac939b679c1601b341589
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-16 21:27:08 +0100

    Tweak css and setup admin nav

--- src/src/admin/index.js
+++ src/src/admin/index.js
@@ -9,8 +9,9 @@ export class Index {
     this.router = router;
     config.title = 'Admin';
     let routes = [
-      { route: ['', 'home'],       name: 'admin-home',       moduleId: 'admin/home/index' }
-    ].concat(this.pluginRoutes);
+      { route: ['', 'home'],       name: 'admin-home',       moduleId: 'admin/home/index', title: 'Admin Home', nav: true, settings:{icon:'fa-dashboard'} }
+    ];
+    routes.concat(this.pluginRoutes);
 
     config.map(routes);
   }
