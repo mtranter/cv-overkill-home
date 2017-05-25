884c6e4be7188ac8fad96baebb9dbc485032a9f4
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-25 21:07:57 +0100

    Add html editor

---
+++ app/src/resources/elements/html-editor/html-editor.js
@@ -0,0 +1,21 @@
+import {bindable, bindingMode, inject} from 'aurelia-framework';
+import $ from 'jquery'
+import summernote from 'summernote'
+import css from 'summernote/dist/summernote.css!text'
+import {DOM} from 'aurelia-pal'
+
+let stylesLoaded = false;
+
+@inject(Element)
+export class HtmlEditorCustomElement {
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) value = ""
+  constructor(element){
+    $(element).summernote();
+  }
+  attached(){
+    if(!stylesLoaded){
+      stylesLoaded = true;
+      DOM.injectStyles(css);
+    }
+  }
+}
--- app/src/resources/index.js
+++ app/src/resources/index.js
@@ -1,3 +1,3 @@
 export function configure(config) {
-  config.globalResources(['./elements/menu/menu','./attributes/date-picker', './value-converters/short-date-value-converter.js']);
+  config.globalResources(['./elements/menu/menu','./attributes/date-picker', './elements/html-editor/html-editor', './value-converters/short-date-value-converter.js']);
 }
