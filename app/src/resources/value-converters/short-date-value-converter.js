092f6efa0f6f65271a1f4a37909f7ab3549e9acb
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-25 13:53:48 +0100

    Add short date converter

--- app/src/resources/value-converters/short-date-value-converter.js
+++ app/src/resources/value-converters/short-date-value-converter.js
@@ -1,6 +1,6 @@
 export class ShortDateValueConverter {
   format = new Intl.DateTimeFormat(navigator.language);
   toView(value) {
-    return this.format.format(value);
+    return this.format.format(Date.parse(value));
   }
 }
