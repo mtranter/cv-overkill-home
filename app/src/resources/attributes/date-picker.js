4c39a7ec1205db064b314af792f3b3502be298d3
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-25 20:51:02 +0100

    Add styles for datepicker

--- app/src/resources/attributes/date-picker.js
+++ app/src/resources/attributes/date-picker.js
@@ -1,7 +1,8 @@
 import {inject} from 'aurelia-framework';
 import $ from 'jquery'
 import datepicker from 'bootstrap-datepicker'
-import 'bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css!text'
+import css from 'bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css!text'
+import {DOM} from 'aurelia-pal'
 
 @inject(Element)
 export class DatePickerCustomAttribute {
@@ -9,5 +10,8 @@ export class DatePickerCustomAttribute {
   constructor(element){
     console.log(datepicker);
     $(element).datepicker({format: "dd/mm/yyyy"});
+  }
+  attached(){
+    DOM.injectStyles(css);
   }
 }
