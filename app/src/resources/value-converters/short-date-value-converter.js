7a45687f5fa90b2a745458710a5b7028021a6384
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-27 22:08:12 +0100

    Fix sort algo

--- app/src/resources/value-converters/short-date-value-converter.js
+++ app/src/resources/value-converters/short-date-value-converter.js
@@ -3,9 +3,9 @@ import dateFormatter from './../../common/date-formatter'
 export class ShortDateValueConverter {
   format = new Intl.DateTimeFormat(navigator.language);
   toView(value) {
-    return dateFormatter.toView(value);
+    return value ? dateFormatter.toView(value) : null;
   }
   fromView(value) {
-    return dateFormatter.fromView(value);
+    return value ? dateFormatter.fromView(value) : null;
   }
 }
