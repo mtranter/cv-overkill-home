2ad17ca77595738228c5b2c03e837dc798a21e00
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-27 19:51:41 +0100

    Add sort fb

--- app/src/resources/index.js
+++ app/src/resources/index.js
@@ -1,3 +1,8 @@
 export function configure(config) {
-  config.globalResources(['./elements/menu/menu','./elements/date-picker/date-picker', './elements/html-editor/html-editor', './value-converters/short-date-value-converter.js']);
+  config.globalResources([
+    './elements/menu/menu',
+    './elements/date-picker/date-picker',
+    './elements/html-editor/html-editor',
+    './value-converters/short-date-value-converter.js',
+    './value-converters/sort.js']);
 }
