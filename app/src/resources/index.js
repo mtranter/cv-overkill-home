8cbe16d29dd88220a3153670720ff1ccb9a0fb67
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-27 09:56:37 +0100

    Fix typo

--- app/src/resources/index.js
+++ app/src/resources/index.js
@@ -1,3 +1,3 @@
 export function configure(config) {
-  config.globalResources(['./elements/menu/menu','./elements/date-pickerz`/date-picker', './elements/html-editor/html-editor', './value-converters/short-date-value-converter.js']);
+  config.globalResources(['./elements/menu/menu','./elements/date-picker/date-picker', './elements/html-editor/html-editor', './value-converters/short-date-value-converter.js']);
 }
