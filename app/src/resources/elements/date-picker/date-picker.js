d00083da2d56678fb0c204e8c53f1a5b2038c0fe
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-27 08:16:21 +0100

    Add date picjer

---
+++ app/src/resources/elements/date-picker/date-picker.js
@@ -0,0 +1,32 @@
+import {inject, bindable, viewResources} from 'aurelia-framework';
+import $ from 'jquery'
+import datepicker from 'bootstrap-datepicker'
+import {DOM} from 'aurelia-pal'
+import dateFormatter from './../../common/date-formatter'
+
+
+@inlineView(`<template>
+    <input ref="editor" value.bind="value" placeholder.bind="placeholder" class.bind="cssClass"></input>
+</template>`)
+@viewResources('bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css')
+@inject(Element)
+export class DatePickerCustomElement {
+  @bindable value;
+  @bindable placeholder;
+  @bindable cssClass;
+  bind(){
+    $(this.editor).datepicker({
+      defaultViewDate: dateFormatter.fromView(this.value),
+      format: {
+          toDisplay:  (date, format, language) => {
+              return dateFormatter.toView(date);
+          },
+          toValue:  (date, format, language) => {
+              return dateFormatter.fromView(date);
+          }
+        }
+      }).on('changeDate', d => {
+        this.value = d.toISOString();
+      });
+  }
+}
