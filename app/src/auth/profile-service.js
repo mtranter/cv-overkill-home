26d6f50f9925700bc9ad24daa833c792a721eb55
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-28 12:13:54 +0100

    Conditional access token

--- app/src/auth/profile-service.js
+++ app/src/auth/profile-service.js
@@ -10,12 +10,9 @@ export class ProfileService {
   getProfile(){
     if(this._profile) return Promise.resolve(this._profile);
     return new Promise((resolve, reject) => {
-      this.http.fetch('https://marktranter.eu.auth0.com/userinfo', {
-        method: "POST",
-        body: JSON.stringify({})
-      }).then(d => {
-        _profile = d.json();
-        resolve(_profile);
+      this.http.fetch('https://marktranter.eu.auth0.com/userinfo').then(d => {
+        this._profile = d.json();
+        resolve(this._profile);
       })
     });
   }
